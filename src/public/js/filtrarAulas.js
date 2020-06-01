f2d2d4f7d8338fcdc6cf40b34243efbc1c7ed411
DSanti001 <DSanti001@users.noreply.github.com>
2020-06-01 19:40:54 -0300

    Funcionando

---
+++ src/public/js/filtrarAulas.js
@@ -0,0 +1,14 @@
+const aulaaulasController = require("../controllers/aulas.constroller.js");
+function filtrar() {
+    const edificio = document.getElementById('edificio').value;
+    const capacidad = document.getElementById('capacidad').value;
+    const ubicacion = document.getElementById('ubicacion').value;
+    alert("hola")
+    if (edificio != "Ninguno") {
+        if (capacidad != "Ninguno") {
+            if (ubicacion != "Ninguno") {
+
+            }
+        }
+    }
+}
